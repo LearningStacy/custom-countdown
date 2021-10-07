c630637bb5f6cf6397319856568761c92e211588
LearningStacy <stacy.saintpaul@gmail.com>
2021-10-07 06:20:50 -0400

    set date input min with today's date (dynamically)

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('test');
+const inputContainer = document.getElementById('input-container');
+const countdownForm = document.getElementById('countdownForm');
+const dateEl = document.getElementById('date-picker');
+
+//Set date input min with today's date
+const today = new Date().toISOString().split('T')[0];
+dateEl.setAttribute('min', today);
+
